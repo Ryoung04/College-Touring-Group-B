20781c688967a790618b6c2e5df8bc06dced2fbb
Reise <147127766+Ryoung04@users.noreply.github.com>
2025-02-07 13:34:34 -0800

    Update mainwindow.cpp

--- tutorial/mainwindow.cpp
+++ tutorial/mainwindow.cpp
@@ -4,6 +4,7 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , mainPage(nullptr)  // Initialize pointer
 {
     ui->setupUi(this);
 }
@@ -15,64 +16,42 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButtonLogin_clicked()
 {
-    QString Username = ui->lineEditUsername->text();
-    QString Password = ui->lineEditPassword->text();
-    if (Username == "oalbuquerque" && Password == "Password123")
+    QString username = ui->lineEditUsername->text();
+    QString password = ui->lineEditPassword->text();
+
+    // List of valid credentials
+    QMap<QString, QString> validCredentials = {
+        {"oalbuquerque", "Password123"},
+        {"aaron", "Password222"},
+        {"linsey", "Password333"},
+        {"reise", "Password444"},
+        {"ferris", "Password555"},
+        {"ruben", "Password666"}
+    };
+
+    if (validCredentials.contains(username) && validCredentials[username] == password)
     {
         QMessageBox::information(this, "Notification", "Login Success.");
         this->hide();
-        MainPage *mainpage = new MainPage();
-        mainpage->show();
-    }
-    if (Username == "aaron" && Password == "Password222")
-    {
-        QMessageBox::information(this, "Notification", "Login Success.");
-        this->hide();
-        MainPage *mainpage = new MainPage();
-        mainpage->show();
-    }
-    if (Username == "linsey" && Password == "Password333")
-    {
-        QMessageBox::information(this, "Notification", "Login Success.");
-        this->hide();
-        MainPage *mainpage = new MainPage();
-        mainpage->show();
-    }
-    if (Username == "reise" && Password == "Password444")
-    {
-        QMessageBox::information(this, "Notification", "Login Success.");
-        this->hide();
-        MainPage *mainpage = new MainPage();
-        mainpage->show();
-    }
-    if (Username == "ferris" && Password == "Password555")
-    {
-        QMessageBox::information(this, "Notification", "Login Success.");
-        this->hide();
-        MainPage *mainpage = new MainPage();
-        mainpage->show();
-    }
-    if (Username == "ruben" && Password == "Password666")
-    {
-        QMessageBox::information(this, "Notification", "Login Success.");
-        this->hide();
-        MainPage *mainpage = new MainPage();
-        mainpage->show();
+
+        if (!mainPage) {
+            mainPage = new MainPage(this);  // Create only one instance
+        }
+        mainPage->show();
     }
     else
     {
-        QMessageBox::information(this, Username, "Please enter valid username or password");
+        QMessageBox::warning(this, "Login Failed", "Please enter a valid username or password.");
     }
 }
-
 
 void MainWindow::on_pushButtonCancel_clicked()
 {
     QMessageBox::StandardButton reply;
-    reply = QMessageBox::question(this, "oalbuquerque", "Are you sure you wante to close the application?", QMessageBox::Yes | QMessageBox::No);
+    reply = QMessageBox::question(this, "Exit", "Are you sure you want to close the application?", QMessageBox::Yes | QMessageBox::No);
+
     if (reply == QMessageBox::Yes)
     {
         QApplication::quit();
     }
 }
-
